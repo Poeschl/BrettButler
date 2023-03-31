381c6892231a8ece64c742cc878f923e0d6663e3
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-03-31 20:59:01 +0200

    🎉 Initial project

---
+++ backend/src/main/kotlin/xyz/poeschl/brettbutler/BrettliApplication.kt
@@ -0,0 +1,11 @@
+package xyz.poeschl.brettbutler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrettliApplication
+
+fun main(args: Array<String>) {
+    runApplication<BrettliApplication>(*args)
+}
