78a5f017771231436204a95828bba866e016e278
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-01 17:01:40 +0200

    :sparkles: First steps with VUE 3 in Typescript

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="is-uppercase has-text-centered is-size-1 mb-4">Brettbuttler</h1>
+
+  <div class="columns">
+    <div class="column">
+      <p class="box has-text-centered">
+        Bringbuttler is a software to makes it easier for you to plan the next board game event.
+        Especially when there are several board games and more people then player seats.
+      </p>
+    </div>
+    <div class="column">
+      <p class="box has-text-centered">
+        Here you can create a new event for an date in the future and select (and manage) a game which is planed.
+        After that, the players can decide on a game and reserve a seat.
+      </p>
+    </div>
+    <div class="column">
+      <div class="box has-text-centered">
+        <p>Built with ❤️ and 🍵 by <a target="_blank" href="//poeschl.xyz">Markus Pöschl</a></p>
+        <p>Source is available on <a target="_blank" href="https://github.com/Poeschl/BrettButtler">GitHub</a></p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
