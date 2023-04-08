58ffe88dcc4642f1ffe53f91c8182410bc43b4bb
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-08 18:27:28 +0200

    :sparkles: Add games detail modal

--- frontend/src/components/GameCard.vue
+++ frontend/src/components/GameCard.vue
@@ -1,45 +1,52 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-96x96">
-            <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt="Placeholder image"
+  <div
+    class="card"
+  >
+    <div
+      class="card-content"
+      @click="$emit('gamecard-clicked', props.game)">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <div
+              class="media"
+
             >
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">
-            {{ props.game.name }}
-          </p>
-          <p class="subtitle mb-1 is-6">
-            {{ props.game.numberOfPlayers }} Player
-          </p>
-          <p class="subtitle mb-1 is-6">
-            {{ props.game.playtimeInMinutes }} Minutes Playtime
-          </p>
-          <div class="level">
-            <div class="level-left">
-              <a class="level-item">
-                <FontAwesomeIcon
-                    class="is-small"
-                    icon="fa-regular fa-pen-to-square"
-                />
-              </a>
+              <div class="media-left">
+                <figure class="image is-96x96">
+                  <img
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                    alt="Placeholder image"
+                  >
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">
+                  {{ props.game.name }}
+                </p>
+                <p class="subtitle mb-1 is-6">
+                  {{ props.game.numberOfPlayers }} Player
+                </p>
+                <p class="subtitle mb-1 is-6">
+                  {{ props.game.playtimeInMinutes }} Minutes Playtime
+                </p>
+              </div>
             </div>
           </div>
+        </div>
+        <div class="level-right">
+          <a class="level-item">
+            <FontAwesomeIcon
+              class="is-small"
+              icon="fa-regular fa-pen-to-square"
+            />
+          </a>
         </div>
       </div>
 
       <p class="content is-description-height has-hidden-overflow">
         {{ props.game.description }}
       </p>
-      <a
-          target="_blank"
-          :href="props.game.url"
-      >More information...</a>
     </div>
   </div>
 </template>
@@ -53,6 +60,8 @@ const props = defineProps<{
   game: Game
 }>()
 
+defineEmits(['gamecard-clicked'])
+
 </script>
 
 <style scoped>
@@ -62,5 +71,13 @@ const props = defineProps<{
 
 .has-hidden-overflow {
   overflow: hidden;
+}
+
+.card-content {
+  cursor: pointer;
+}
+
+.title {
+  margin-bottom: revert;
 }
 </style>
