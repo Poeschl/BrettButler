175a0c67226c39e03732ba8da80afffc958bb9d0
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-08 18:36:01 +0200

    :sparkles: Add new game button

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,12 +5,13 @@ import router from "./router";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faPenToSquare} from "@fortawesome/free-regular-svg-icons";
-import {faDice} from "@fortawesome/free-solid-svg-icons";
+import {faDice, faPlus} from "@fortawesome/free-solid-svg-icons";
 
 const app: Application<Element> = createApp(App)
 
 library.add(faPenToSquare)
 library.add(faDice)
+library.add(faPlus)
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.use(router)
