3fe9bc644f49ba64a27d7bfc58f7d51bccc971e3
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-08 18:36:01 +0200

    :sparkles: Add new game button

--- frontend/src/views/GamesView.vue
+++ frontend/src/views/GamesView.vue
@@ -1,4 +1,23 @@
 <template>
+  <div class="level">
+    <div class="level-left"/>
+    <div class="level-right">
+      <div class="level-item">
+        <button
+          class="button is-primary"
+          @click="openEditModal(null)"
+        >
+          <span class="icon">
+            <FontAwesomeIcon
+              class="is-small"
+              icon="fa-solid fa-plus"
+            />
+          </span>
+          <span>Add new game</span>
+        </button>
+      </div>
+    </div>
+  </div>
   <div class="columns is-multiline">
     <div
       v-for="game in gameList"
@@ -7,7 +26,7 @@
     >
       <GameCard
         :game="game"
-        @gamecard-clicked="openModal"
+        @gamecard-clicked="openDetailModal"
       />
     </div>
   </div>
@@ -26,6 +45,7 @@ import GameCard from '../components/GameCard.vue'
 import GameDetailsModal from "../components/GameDetailsModal.vue";
 import Game from "../models/Game";
 import {ref} from "vue";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 const gameService = new GameService()
 
@@ -33,14 +53,23 @@ const gameList = gameService.getAllGames()
 
 const modalGame = ref<Game>(gameList[0])
 const showDetails = ref(false)
+const showEdit = ref(false)
 
-const openModal = (game: Game) => {
+const openDetailModal = (game: Game) => {
   modalGame.value = game
   showDetails.value = true
 }
 
 const closeModal = () => {
   showDetails.value = false
+  showEdit.value = false
+}
+
+const openEditModal = (game: Game | null) => {
+  if (game !== null) {
+    modalGame.value = game
+  }
+  showEdit.value = true
 }
 
 </script>
