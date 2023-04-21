02be6a44c918987a56f63a5f505208fa0a6cf199
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-21 15:23:19 +0200

    :recycle: Generalise modals

---
+++ frontend/src/components/modals/GeneralModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="modal is-active"
+  >
+    <div
+      class="modal-background"
+    />
+    <div class="modal-content">
+      <div class="card">
+        <div class="card-header">
+          <slot name="header"/>
+        </div>
+
+        <div class="card-content form">
+          <slot name="content"/>
+        </div>
+        <slot name="footer"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
