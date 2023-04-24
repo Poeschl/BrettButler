c39fddb8a475c78b69017fcb0b876249745b302a
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-04-24 23:09:04 +0200

    :sparkles: Make navbar mobile save

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav
-      class="navbar is-primary is-radiusless"
-      role="navigation"
+    class="navbar is-primary is-radiusless"
+    role="navigation"
   >
     <div class="container">
       <div class="navbar-brand">
@@ -15,18 +15,34 @@
           />
           <span class="is-uppercase">BrettButtler</span>
         </a>
+        <a
+          role="button"
+          class="navbar-burger"
+          data-target="menuItems"
+          :class="{ 'is-active': mobileNavOpen }"
+          @click="toggleMobileNav"
+        >
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+        </a>
       </div>
-      <div class="navbar-menu">
+
+      <div
+        id="menuItems"
+        class="navbar-menu"
+        :class="{ 'is-active': mobileNavOpen }"
+      >
         <div class="navbar-end">
           <router-link
-              class="navbar-item"
-              to="/events"
+            class="navbar-item"
+            to="/events"
           >
             Events
           </router-link>
           <router-link
-              class="navbar-item"
-              to="/games"
+            class="navbar-item"
+            to="/games"
           >
             Games
           </router-link>
@@ -35,6 +51,13 @@
     </div>
   </nav>
 </template>
+
 <script setup lang="ts">
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {ref} from "vue";
+
+const mobileNavOpen = ref<boolean>(false)
+const toggleMobileNav = () => {
+  mobileNavOpen.value = !mobileNavOpen.value
+}
 </script>
